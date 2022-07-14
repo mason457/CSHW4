01d2d920ba46bc722aaf84b242581a45b49cac4d
mason457 <yscar6@gmail.com>
2022-07-14 11:09:25 +0800

    加入專案檔案。

---
+++ HW4/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace HW4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 355);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(110, 50);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "模式";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(60, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Tag = "stopwatch";
+            this.radioButton2.Text = "碼表";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Tag = "clock";
+            this.radioButton1.Text = "時鐘";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "重新開始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 412);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HW4/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime;
+
+namespace HW4
+{
+    public partial class Form1 : Form
+    {
+        private Graphics m_graphic;
+        private Timer m_timer1;
+        private Timer m_timer2;
+        private float m_width;
+        private float m_height;
+        private Color m_bgcolor;     //背景
+        private Color m_backcolor;   //內圈
+        private Color m_scalecolor;  //刻度
+        private Color m_seccolor;    //秒針
+        private Color m_mincolor;    //分針
+        private Color m_hourcolor;   //時針
+        private float m_radius;      //半徑
+        private Color m_mseccolor;    //豪秒針
+
+        int secend;
+        int minute;
+        int hour;
+
+        int cmsecend = 0;
+        int csecend = 0;
+        int cminute = 0;
+        int chour = 0;
+
+        System.DateTime TimeNow = new DateTime();
+        TimeSpan TimeCount = new TimeSpan();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button1.Visible = false;
+                button2.Visible = false;
+                label1.Visible = true;
+                label2.Visible = false;
+                label1.Text = string.Format("{0:00}:{1:00}:{2:00}", DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            }
+            else if(radioButton2.Checked == true)
+            {
+                button1.Visible = true;
+                button2.Visible = true;
+                label1.Visible = false;
+                label2.Visible = true;
+                label2.Text = string.Format("{0:00}:{1:00}:{2:00}:{3:000}", TimeCount.Hours, TimeCount.Minutes, TimeCount.Seconds, TimeCount.Milliseconds);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer,true);
+
+            m_timer1 = new Timer();
+            m_timer1.Interval = 1000;
+            m_timer1.Enabled = true;
+            m_timer1.Tick += new EventHandler(timer1_Tick);
+
+            m_timer2 = new Timer();
+            m_timer2.Interval = 1;
+            m_timer2.Tick += new EventHandler(timer2_Tick);
+
+            m_width = this.ClientSize.Width;
+            m_height = this.ClientSize.Height;
+            m_bgcolor = Color.AliceBlue;
+            m_backcolor = Color.Gray;
+            m_scalecolor = Color.Gray;
+            m_seccolor = Color.Red;
+            m_mincolor = Color.Blue;
+            m_hourcolor = Color.Green;
+            m_mseccolor = Color.Black;
+
+            if (m_width > m_height)
+            {
+                m_radius = (float)(m_height - 8) / 2;
+            }
+            else
+            {
+                m_radius = (float)(m_width - 8) / 2;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            m_graphic = e.Graphics;
+            //座標原點
+            m_graphic.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            m_graphic.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            //外邊框
+            m_graphic.TranslateTransform((float)(m_width / 2), (float)(m_height / 2));
+            m_graphic.FillEllipse(new SolidBrush(m_bgcolor), -m_radius, -m_radius, m_radius * 2, m_radius * 2);
+            //內邊框
+            Pen pen = new Pen(m_bgcolor, 2);
+            m_graphic.DrawEllipse(pen, m_radius * (-1), m_radius * (-1), m_radius * 2, m_radius * 2);
+            //小刻度
+            for (int i = 0; i < 60; i++)
+            {
+                m_graphic.FillRectangle(new SolidBrush(m_scalecolor), -3, 2 - m_radius, 6, 18);
+                m_graphic.RotateTransform(6);
+            }
+            //大刻度
+            for (int i = 0; i < 12; i++)
+            {
+                m_graphic.FillRectangle(new SolidBrush(m_scalecolor), -2, 2 - m_radius, 4, 10);
+                m_graphic.RotateTransform(30);
+            }
+
+            if(radioButton1.Checked == true)
+            {
+                //獲取時間
+                int msecend = DateTime.Now.Millisecond;
+                secend = DateTime.Now.Second;
+                minute = DateTime.Now.Minute;
+                hour = DateTime.Now.Hour;
+
+                //秒針
+                pen.Color = m_seccolor;
+                m_graphic.RotateTransform(6 * secend);
+                m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 1.5));
+                //分針
+                pen.Color = m_mincolor;
+                m_graphic.RotateTransform(6 * secend * (-1));
+                m_graphic.RotateTransform((float)(6 / 60 * secend + 6 * minute));
+                m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 2));
+                //時針
+                pen.Color = m_hourcolor;
+                m_graphic.RotateTransform((float)(6 / 60 * secend + 6 * minute) * (-1));
+                m_graphic.RotateTransform((float)(30 / 3600 * secend + 30 / 60 * minute + hour * 30));
+                m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 2.5));
+            }
+            else if(radioButton2.Checked == true)
+            {
+                pen.Color = m_mseccolor;
+
+                if(button1.Text == "開始")
+                {
+                    cmsecend = 0;
+                    csecend = 0;
+                    cminute = 0;
+                    chour = 0;
+                }
+                else
+                {
+                    cmsecend = TimeCount.Milliseconds;
+                    csecend = TimeCount.Seconds;
+                    cminute = TimeCount.Minutes;
+                    chour = TimeCount.Hours;
+                }
+                
+                m_graphic.RotateTransform((float)(0.36 * cmsecend));
+                m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 1));
+
+                pen.Color = m_seccolor;
+                m_graphic.RotateTransform((float)(0.36 * cmsecend) * (-1));
+                m_graphic.RotateTransform((float)(0.36 / 60 * cmsecend + 6 * csecend));
+                m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 1.5));
+
+                pen.Color = m_mincolor;
+                m_graphic.RotateTransform((float)(0.36 / 60 * cmsecend + 6 * csecend) * (-1));
+                m_graphic.RotateTransform((float)(0.36 / 3600 * cmsecend + 6/60 * csecend + 6 * cminute));
+                m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 2));
+
+                pen.Color = m_hourcolor;
+                m_graphic.RotateTransform((float)(0.36 / 3600 * cmsecend + 6 / 60 * csecend + 6 * cminute) * (-1));
+                m_graphic.RotateTransform((float)(30 / 36000000 * cmsecend + 30 / 3600 * csecend + 30 / 60 * cminute + chour * 30));
+                m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 2.5));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "開始")
+            {
+                button1.Text = "暫停";
+                timer2.Start();
+                TimeNow = DateTime.Now;
+            }
+            else if (button1.Text == "暫停")
+            {
+                button1.Text = "繼續";
+                timer2.Stop();
+            }
+            else if(button1.Text == "繼續")
+            {
+                button1.Text = "暫停";
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+            TimeCount = DateTime.Now - TimeNow;
+            label2.Text = string.Format("{0:00}:{1:00}:{2:00}:{3:000}", TimeCount.Hours, TimeCount.Minutes, TimeCount.Seconds, TimeCount.Milliseconds);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            label2.Text = string.Format("{0:00}:{1:00}:{2:00}:{3:000}", 0, 0, 0, 0);
+            cmsecend = 0;
+            csecend = 0;
+            cminute = 0;
+            chour = 0;
+            timer2.Enabled = false;
+            button1.Text = "開始";
+        }
+    }
+}
